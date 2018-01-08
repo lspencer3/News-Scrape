090909275f391329e8eba666aa2d61bdd4864589
Lauren Spencer <laurenspencer@Laurens-MacBook-Pro.local>
2018-01-08 17:39:59 -0500

    update

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 $.ajax({
     method: "GET",
     url: "/articles"
@@ -6,18 +7,48 @@ $.ajax({
 .done(function(data) {
       console.log(data)
       for (var i = 0; i < data.length; i++){
+        var newDiv = $("<div>")
         var newATag = $("<a>")
         var titleP = $("<p>")
-        var titlePtxt = data[i].title
+        var title = data[i].title
         var link = data[i].link
 
-        titleP.html(titlePtxt)
+        var notesBut = $("<button>")
+        notesBut.html("Add Notes")
+        notesBut.attr("class","notes")
+        notesBut.attr("id",i)
+
+        var saveBut = $("<button>")
+        saveBut.html("Save Article")
+        saveBut.attr("id","save")
+
+        titleP.html(title)
         newATag.attr("href",link)
 
         newATag.append(titleP)
-        $("#articles").prepend(newATag)
+        newDiv.append(newATag)
+        newDiv.append(saveBut)
+        newDiv.append(notesBut)
+        $("#articles").prepend(newDiv)
       }
     });
+
+$(document).on("click", ".notes", function() {
+    var modal = document.getElementById("myModal")
+    modal.style.display = "block"
+
+    var span = document.getElementById("close")
+    span.onclick = function() {
+    modal.style.display = "none";
+
+  }
+
+})
+
+var submit = document.getElementById("submit")
+    submit.onclick = function(){
+      console.log("hello")
+    }
 
   // Whenever someone clicks scrape button
 $(document).on("click", "#scrape", function() {
